569a7bddf375a38716d71ed3f012584d6068b21e
dimdred <dimdred91@gmail.com>
2020-04-14 15:47:00 -0400

    Form Events & PreventDefault

--- events.js
+++ events.js
@@ -15,3 +15,17 @@ newBtn.addEventListener('mouseover', () => newBtn.innerText = 'Mouseover Button!
 newBtn.addEventListener('mouseout', () => newBtn.innerText = 'New Button!')
 
 window.addEventListener('scroll', () => console.log('Stop scrolling!'))
+
+const cCardInput = document.querySelector('#cc');
+const termsCheckBox = document.querySelector('#terms');
+const veggieSelect = document.querySelector('#veggie');
+
+const form = document.querySelector('#signup-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Submitted the form!');
+    console.log(cCardInput.value);
+    console.log(terms.checked);
+    console.log(veggieSelect.value);
+    // send form data to db
+})
