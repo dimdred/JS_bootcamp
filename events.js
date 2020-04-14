fe1b20e38be801abe490285b46b54b72c41928e2
dimdred <dimdred91@gmail.com>
2020-04-13 20:31:45 -0400

    addEventListener

--- events.js
+++ events.js
@@ -1,3 +1,17 @@
 const btn = document.querySelector('#clicker');
 btn.onclick = () => (console.log('Clicker clicked!'))
 btn.ondblclick = () => (console.log('Clicker double clicked!'))
+
+const newBtn = document.querySelector('#newBtn');
+
+newBtn.addEventListener('click', function() {
+    console.log('Listener!')
+})
+
+newBtn.addEventListener('click', () => alert('Second Listener!'));
+
+newBtn.addEventListener('mouseover', () => newBtn.innerText = 'Mouseover Button!')
+
+newBtn.addEventListener('mouseout', () => newBtn.innerText = 'New Button!')
+
+window.addEventListener('scroll', () => console.log('Stop scrolling!'))
