2cf604c72c923da01efccfaecd3435dddb051a24
dimdred <dimdred91@gmail.com>
2020-04-13 21:48:20 -0400

    events on multiply elements

--- impBtn.js
+++ impBtn.js
@@ -11,3 +11,19 @@ impBtn.addEventListener('click', () => {
     impBtn.innerText = 'You got me!'
     document.body.style.backgroundColor = 'green';
 })
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'indigo', 'violet'];
+// arrow function doesn't work (this doesn't work properly)
+const h1 = document.querySelector('h1');
+const changeColor = function() {
+    h1.style.color = this.style.backgroundColor;
+    console.log(this.style.backgroundColor);
+}
+const boxes = document.querySelector('#boxes');
+for(let color of colors) {
+    const box = document.createElement('div');
+    box.style.backgroundColor = color;   
+    box.classList.add('box');
+    boxes.appendChild(box);
+    box.addEventListener('click', changeColor);
+}
