60aaa2883708142a3af2cd7631d04057f45f8a3c
dimdred <dimdred91@gmail.com>
2020-04-19 00:52:57 -0400

    multiple awaits

--- callStack/refactoringPromises.js
+++ callStack/refactoringPromises.js
@@ -1,43 +1,54 @@
 const btn = document.querySelector("#bt");
 
 const moveX = (element, amount, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const bodyBoundary = document.body.clientWidth;
-            const elRight = element.getBoundingClientRect().right;
-            const currLeft = element.getBoundingClientRect().left;
-        
-            if (elRight + amount > bodyBoundary) {
-              reject({bodyBoundary, elRight, amount});
-            } else {
-              element.style.transform = `translateX(${currLeft + amount}px)`;
-              resolve(`${currLeft + amount}`);
-            }
-          }, delay);
-    })
-  };
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
 
-  moveX(btn, 300, 3000)
-    .then(() => moveX(btn, 300, 3000))
-    .then(() => moveX(btn, 300, 3000)) 
-    .then(() => {
-        console.log('Done moving!');
-        return moveX(btn, 300, 3000);
-    })
-    .then(() => {
-        console.log('Done moving 4!');
-    })
-    .catch(({bodyBoundary, elRight, amount}) => {
-        console.log(`Body is ${bodyBoundary}px wide`);
-        console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
-    })
+      if (elRight + amount > bodyBoundary) {
+        reject({ bodyBoundary, elRight, amount });
+      } else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        resolve(`${currLeft + amount}`);
+      }
+    }, delay);
+  });
+};
+
+async function animateRight(el, amt) {
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+  await moveX(el, amt, 1000);
+}
+animateRight(btn, 300).catch((err) => {
+  console.log('Change direction!');
+  animateRight(btn, -300)
+});
+
+// moveX(btn, 300, 3000)
+//   .then(() => moveX(btn, 300, 3000))
+//   .then(() => moveX(btn, 300, 3000))
+//   .then(() => {
+//       console.log('Done moving!');
+//       return moveX(btn, 300, 3000);
+//   })
+//   .then(() => {
+//       console.log('Done moving 4!');
+//   })
+//   .catch(({bodyBoundary, elRight, amount}) => {
+//       console.log(`Body is ${bodyBoundary}px wide`);
+//       console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+//   })
 
 // const moveX = (element, amount, delay, onSuccess, onFailure) => {
 //     setTimeout(() => {
 //       const bodyBoundary = document.body.clientWidth;
 //       const elRight = element.getBoundingClientRect().right;
 //       const currLeft = element.getBoundingClientRect().left;
-  
+
 //       if (elRight + amount > bodyBoundary) {
 //         onFailure();
 //       } else {
