f55a826fe1f36d856fd9c986c6bcd22b78979577
dimdred <dimdred91@gmail.com>
2020-04-18 23:20:16 -0400

    await

--- async/async.js
+++ async/async.js
@@ -11,22 +11,48 @@ greet()
   .catch((err) => console.log(err));
 
 async function add(x, y) {
-    if(typeof x !== 'number' || typeof y !== 'number') {
-      throw 'Incorrect data type!';
-    }
-    return x + y;
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw "Incorrect data type!";
   }
-
- // the same logic without async 
-function add2(a,b) {
-  return new Promise((resolve, reject) => {
-    if(typeof a !== 'number' || typeof b !== 'number') {
-      reject('Incorrect data type!');
-    }
-    resolve(a + b);
-  })
+  return x + y;
 }
 
-  add(5,'r')
+// the same logic without async
+function add2(a, b) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("Incorrect data type!");
+    }
+    resolve(a + b);
+  });
+}
+
+add(5, 9)
   .then((response) => console.log(response))
   .catch((err) => console.log(err));
+
+// function getCharacter() {
+//   return axios.get("https://anapioficeandfire.com/api/characters/");
+// }
+
+// getCharacter().then((res) => console.log(res.data));
+
+
+// the same logic with async and await
+async function getCharacter() {
+  const result = await axios.get("https://anapioficeandfire.com/api/characterss/");
+  console.log(result.data);
+}
+
+getCharacter().catch(err => console.log('IN CATCH!', err));
+
+async function getCharacterNew() {
+  try {
+    const result = await axios.get("https://anapioficeandfire.com/api/characterss/");
+    console.log(result.data);
+  } catch (err) {
+    console.log('IN CATCH NEW!', err);
+  }
+}
+
+getCharacterNew();
