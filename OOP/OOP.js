653f34c08e984fad57f8bc976dcb35e1dec1321e
dimdred <dimdred91@gmail.com>
2020-04-21 00:16:30 -0400

    Constructor functions

--- OOP/OOP.js
+++ OOP/OOP.js
@@ -6,28 +6,52 @@ Array.prototype.pop = function () {
   return "pop was broken!";
 };
 
-function hex(r, g, b) {
-  return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
+// function hex(r, g, b) {
+//   return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// function rgb(r, g, b) {
+//   return `rgb(${r}, ${g}, ${b})`;
+// }
+
+// function makeColor(r, g, b) {
+//   const color = {};
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+//   color.rgb = function() {
+//     const {r, g, b} = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   }
+//   color.hex = function() {
+//     const {r, g, b} = this;
+//     return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
+//   }
+//   return color;
+// }
+
+// const c = makeColor(255, 100, 25)
+
+function Color(r, g, b) {
+  this.r = r;
+  this.g = g;
+  this.b = b;
 }
 
-function rgb(r, g, b) {
+Color.prototype.rgb = function () {
+  const { r, g, b } = this;
   return `rgb(${r}, ${g}, ${b})`;
+};
+
+Color.prototype.hex = function () {
+  const { r, g, b } = this;
+  return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+Color.prototype.rgba = function (a=1) {
+  const { r, g, b } = this;
+  return `rgb(${r}, ${g}, ${b}, ${a})`;
 }
 
-function makeColor(r, g, b) {
-  const color = {};
-  color.r = r;
-  color.g = g;
-  color.b = b;
-  color.rgb = function() {
-    const {r, g, b} = this;
-    return `rgb(${r}, ${g}, ${b})`;
-  }
-  color.hex = function() {
-    const {r, g, b} = this;
-    return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
-  }
-  return color;
-}
-
-const c = makeColor(255, 100, 25)
+const color1 = new Color(255, 0, 0);
+const color2 = new Color(0, 0, 0);
