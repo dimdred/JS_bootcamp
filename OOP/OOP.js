9ad327848fda1cbbd54ef8298a3fd617e7c53baa
dimdred <dimdred91@gmail.com>
2020-04-20 23:44:38 -0400

    factory functions

--- OOP/OOP.js
+++ OOP/OOP.js
@@ -1,7 +1,33 @@
-String.prototype.yell = function() {
-  return `OMG ${this.toUpperCase()} !!!!`
+String.prototype.yell = function () {
+  return `OMG ${this.toUpperCase()} !!!!`;
+};
+
+Array.prototype.pop = function () {
+  return "pop was broken!";
+};
+
+function hex(r, g, b) {
+  return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-Array.prototype.pop = function() {
-  return 'pop was broken!'
+function rgb(r, g, b) {
+  return `rgb(${r}, ${g}, ${b})`;
 }
+
+function makeColor(r, g, b) {
+  const color = {};
+  color.r = r;
+  color.g = g;
+  color.b = b;
+  color.rgb = function() {
+    const {r, g, b} = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  color.hex = function() {
+    const {r, g, b} = this;
+    return "#" + ((1 << 24) + (r + 16) + (g << 8) + b).toString(16).slice(1);
+  }
+  return color;
+}
+
+const c = makeColor(255, 100, 25)
