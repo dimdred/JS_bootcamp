e409373f3e012fc507868b7a553152e5d71aa7b9
dimdred <dimdred91@gmail.com>
2020-04-23 00:36:44 -0400

    timer initial commit

---
+++ Part 2/timer/index.js
@@ -0,0 +1,19 @@
+class Timer {
+    constructor(durationInput, startBtn, pauseBtn) {
+        this.durationInput = durationInput;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
+
+        this.startBtn.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Start timer!');
+    }
+}
+
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
