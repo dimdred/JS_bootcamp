12d2fcfd4009d8255c1e1e8f80857eafeee0e759
dimdred <dimdred91@gmail.com>
2020-04-30 22:21:01 -0400

    this solving

--- Part 2/timer/index.js
+++ Part 2/timer/index.js
@@ -4,11 +4,15 @@ class Timer {
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
 
-        this.startBtn.addEventListener('click', this.start);
+        this.startBtn.addEventListener('click', this.start); //this.start.bind(this)
     }
 
-    start() {
-        console.log('Start timer!');
+    start = () => {
+        this.important();
+    }
+
+    important() {
+        console.log('IMPORTANT!');
     }
 }
 
@@ -17,3 +21,38 @@ const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startBtn, pauseBtn);
+// timer.start();
+
+// using arrow function
+// console.log(this);
+// const printThis = () => console.log(this);
+// printThis();
+
+// const colors = {
+//     printColor() {
+//         console.log(this);
+//         const printThis = () => console.log(this);
+//         printThis();
+//     }
+// }
+// colors.printColor();
+
+// using call, apply methods
+// const printThis = function() {
+//     console.log(this)
+// }
+// printThis.call({color: 'red'}); // the same with apply - override this
+
+
+// dot rule: this belongs to the left of the '.' in the method call
+const colors = {
+    printColor() {
+        console.log(this);
+    }
+}
+
+const randomObj = {
+    a: 1
+};
+randomObj.printColor = colors.printColor;
+randomObj.printColor(); // this is randomObj
