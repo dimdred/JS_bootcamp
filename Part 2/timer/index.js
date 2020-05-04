09de154359c608231443d5ce4b1fd16d95ae45cc
dimdred <dimdred91@gmail.com>
2020-05-04 00:00:18 -0400

    animation

--- Part 2/timer/index.js
+++ Part 2/timer/index.js
@@ -1,13 +1,18 @@
 const durationInput = document.querySelector("#duration");
 const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
+const circle = document.querySelector('circle');
+const perimetr = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimetr);
 
+let currentOffset = 0;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
   onStart() {
     console.log("Timer started!");
   },
   onTick() {
-    console.log("Timer just ticked down!");
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 1;
   },
   onComplete() {
     console.log("Timer is completed!");
